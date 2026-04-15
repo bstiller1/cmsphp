1e082beb9ac19d136dcbe8d5ce8f25c52d5fef09
bstiller1 <stiller.blake@gmail.com>
2026-04-15 11:21:34 -0400

    Added .gitignore, removing 'db.php' and 'setup.php'

--- setup.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('db.php');
-$hashed_password = password_hash('SwordFish!123', PASSWORD_DEFAULT);
-$sql = "UPDATE users SET password = '$hashed_password' WHERE username = 'admin'";
-mysqli_query($conn, $sql);
-echo "Admin user created!";
-?>
